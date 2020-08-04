fbf9a0b86282a9bcd01a17a5f5dec2e55797ddcb
Lucian Sturião <luciansr@gmail.com>
2020-08-04 10:04:47 -0300

    Add Code Service

--- src/services/CodeService.ts
+++ src/services/CodeService.ts
@@ -1,8 +1,17 @@
-
+import Peer from 'simple-peer';
 
 export class CodeService {
+
+    private async Test() {
+        
+    }
+
     static async GetNewCodeInterviewCode(): Promise<string> {
         return new Promise((resolve) => {
+
+            
+            var peer = new Peer();
+
             resolve("asd")
         })
       }
